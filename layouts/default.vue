1726271e8920ce24210b76550120244cdff1fab7
mizu <psdk55052004@gmail.com>
2025-01-09 00:37:53 +0800

    修正 移除 nuxt-aos 產生的X軸

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup lang="ts">
+// 解決 aos 產生的X軸
+function removeXAxosGeneratedByAos() {
+  document.documentElement.style.overflowX = "hidden";
+  document.body.style.overflowX = "hidden";
+}
+
+onMounted(() => {
+  removeXAxosGeneratedByAos();
+});
+</script>
 
 <template>
   <div>
