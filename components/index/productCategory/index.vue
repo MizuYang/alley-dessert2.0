f2eb0a74f4bd11aa41cd7ea2dbc338278f3d8558
mizu <psdk55052004@gmail.com>
2025-01-13 20:41:16 +0800

    新增 產品類別輪播、store

---
+++ components/index/productCategory/index.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h3 class="text-primary pb-12 text-center text-[32px]">產品類別</h3>
+    <div class="border-primary border-b border-t border-solid py-12">
+      <div class="mx-auto grid max-w-[1200px] grid-cols-3">
+        <IndexProductCategoryCakeSwiper />
+        <IndexProductCategoryProfiteroleSwiper />
+        <IndexProductCategoryPuddingSwiper />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- stores/productStore.js
+++ stores/productStore.js
@@ -1,27 +1,38 @@
 import { defineStore } from "pinia";
 
-export const useProductStore = defineStore(
-  "useProductStore",
-  () => {
-    const env = useRuntimeConfig();
-    const products = ref([]);
-    const popularProducts = computed(() => products.value.filter((item) => item.popular >= 3))
+export const useProductStore = defineStore("useProductStore", () => {
+  const env = useRuntimeConfig();
+  const products = ref([]);
+  const popularProducts = computed(() =>
+    products.value.filter((item) => item.popular >= 3),
+  );
+  const cakeProducts = computed(() =>
+    products.value.filter((item) => item.category === "蛋糕"),
+  );
+  const puddingProducts = computed(() =>
+    products.value.filter((item) => item.category === "布丁"),
+  );
+  const profiteroleProducts = computed(() =>
+    products.value.filter((item) => item.category === "泡芙"),
+  );
 
-    async function getProducts() {
-      const api = `${env.public.apiBaseUrl}/api/${env.public.apiPath}/products/all`;
+  async function getProducts() {
+    const api = `${env.public.apiBaseUrl}/api/${env.public.apiPath}/products/all`;
 
-      try {
-        const res = await $fetch(api);
-        products.value = res.products;
-      } catch (err) {
-        console.error(err);
-      }
+    try {
+      const res = await $fetch(api);
+      products.value = res.products;
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    return {
-      popularProducts,
-      products,
-      getProducts,
-    };
-  },
-);
+  return {
+    cakeProducts,
+    profiteroleProducts,
+    popularProducts,
+    puddingProducts,
+    products,
+    getProducts,
+  };
+});
