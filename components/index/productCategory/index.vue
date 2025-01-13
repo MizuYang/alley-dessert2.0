3a7d2dcb24921f30b2b2f537a8e2ec56ed1f85f2
mizu <psdk55052004@gmail.com>
2025-01-13 21:20:13 +0800

    新增 產看所有產品按鈕

--- components/index/productCategory/index.vue
+++ components/index/productCategory/index.vue
@@ -5,10 +5,11 @@ const { cakeProducts, puddingProducts, profiteroleProducts } =
 
 <template>
   <div>
-    <h3 class="text-primary pb-12 text-center text-[32px]">產品類別</h3>
-    <div class="border-primary border-b border-t border-solid py-12">
-      <div class="mx-auto grid max-w-[1200px] grid-cols-3 gap-20">
-
+    <h3 class="text-primary pb-5 text-center text-[32px]">產品類別</h3>
+    <div
+      class="border-primary border-b border-t border-solid py-12 text-center"
+    >
+      <div class="mx-auto mb-14 grid max-w-[1200px] grid-cols-3 gap-20">
         <IndexProductSwiper
           title="蛋糕"
           :slidesPerView="1"
@@ -28,6 +29,13 @@ const { cakeProducts, puddingProducts, profiteroleProducts } =
           :products="profiteroleProducts"
         />
       </div>
+      <NuxtLink
+        to="/products"
+        class="text-primary border-primary hover:bg-primary/20 inline-block border border-solid px-3 py-3 text-2xl hover:scale-[1.01]"
+        @click="currentPage('products')"
+      >
+        <i class="bi bi-shop"></i> 查看所有產品
+      </NuxtLink>
     </div>
   </div>
 </template>
