b2f3db8157f39376ca6c40b048c290fe9bb7d437
mizu <psdk55052004@gmail.com>
2025-01-15 01:26:34 +0800

    新增 頁面標題元件

---
+++ components/pageTitle.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="text-primary">
+    <h2 class="mb-12 text-center">
+      <span
+        class="before:bg-primary after:bg-primary relative text-[40px] before:absolute before:right-full before:top-1/2 before:h-[1px] before:w-[120px] before:-translate-x-16 after:absolute after:left-full after:top-1/2 after:h-[1px] after:w-[120px] after:translate-x-16"
+        >{{ props.title }}</span
+      >
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- pages/products/[filterCategory]/index.vue
+++ pages/products/[filterCategory]/index.vue
@@ -9,12 +9,8 @@ onMounted(() => {
 
 <template>
   <main class="text-primary bg-black pb-14 pt-10">
-    <h2 class="mb-12 text-center">
-      <span
-        class="before:bg-primary after:bg-primary relative text-[40px] before:absolute before:right-full before:top-1/2 before:h-[1px] before:w-[120px] before:-translate-x-16 after:absolute after:left-full after:top-1/2 after:h-[1px] after:w-[120px] after:translate-x-16"
-        >精選產品</span
-      >
-    </h2>
+    <PageTitle :title="'精選產品'" />
+
 
     <ProductsFilterSearchSortTool />
 
