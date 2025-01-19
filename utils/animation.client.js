932d66b7799228fec4a0466fc15f8a43cfd8f9bb
mizu <psdk55052004@gmail.com>
2025-01-20 01:03:44 +0800

    新增 將 animate 模組化

---
+++ utils/animation.client.js
@@ -0,0 +1,44 @@
+/**
+ * https://animate.style/
+ */
+import "animate.css";
+
+/**
+ * 
+import { addAnimation } from "~/utils/animation.client.js";
+
+const addAnimationFn = ({ element, animateName }) => {
+  const fn = addAnimation({ element, animateName });
+  if (typeof fn === "function") fn();
+};
+ */
+
+export function addAnimation({ element, animateName }) {
+  if (!element || !animateName || import.meta.server) return;
+
+  let isAnimationEnd = false;
+  let isListening = false;
+
+  return function () {
+    try {
+      if (isAnimationEnd) {
+        element.classList.remove(`animate__${animateName}`);
+        isAnimationEnd = false;
+        return;
+      }
+      isAnimationEnd = true;
+      element.classList.add(`animate__${animateName}`);
+
+      // 若沒有監聽過，則監聽一次
+      if (!isListening) {
+        window.addEventListener("animationend", () => {
+          isAnimationEnd = true;
+          isAnimationEnd = false;
+          element.classList.remove(`animate__${animateName}`);
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+}
