ac90ea75b6871c5580afea6e8763ffb86505cd42
mizu <psdk55052004@gmail.com>
2025-01-20 00:17:01 +0800

    新增 訂單編號: 切版、按鈕、輸入框驗證失敗動畫

---
+++ components/order/form.vue
@@ -0,0 +1,74 @@
+<script setup>
+import "animate.css";
+
+const orderId = ref("");
+const orderInput = ref(null);
+
+const isOrderIdInputDisabled = computed(() => orderId.value.length === 20);
+const inputClass = computed(() => {
+  return isOrderIdInputDisabled.value
+    ? "ring-gray-400 focus:ring-gray/50 "
+    : "ring-red-500 focus:ring-red/50";
+});
+const btnClass = computed(() =>
+  isOrderIdInputDisabled.value
+    ? "bg-red-800 hover:bg-red-800/90 active:bg-red-800/80"
+    : "bg-gray-400 hover:bg-gray-400/90 active:bg-gray-400/80 cursor-not-allowed animate__animated  animate__shakeX",
+);
+const addErrorAnimationFn = addErrorAnimation();
+
+function getOeder() {
+  if (!isOrderIdInputDisabled.value) {
+    if (orderInput.value) orderInput.value.focus();
+    addErrorAnimationFn();
+    return;
+  }
+}
+function addErrorAnimation() {
+  let isAnimationEnd = false;
+
+  return function () {
+    if (isAnimationEnd) {
+      orderInput.value.classList.remove("animate__animated", "animate__shakeX");
+      isAnimationEnd = false;
+      return;
+    }
+    isAnimationEnd = true;
+    orderInput.value.classList.add("animate__animated", "animate__shakeX");
+
+    setTimeout(() => {
+      isAnimationEnd = false;
+      orderInput.value.classList.remove("animate__animated", "animate__shakeX");
+    }, 1000);
+  };
+}
+</script>
+
+<template>
+  <form
+    class="border-primary rounded-3xl border-2 border-solid px-16 py-10 text-center"
+  >
+    <h3 class="text-primary mb-8 text-[32px]">請輸入訂單編號( 20碼 )</h3>
+    <input
+      type="text"
+      class="mb-4 w-full bg-gray-700/80 px-3 py-2 text-xl ring-1 focus:outline-none focus:ring-4"
+      :class="inputClass"
+      placeholder="請輸入訂單編號 (20碼)"
+      v-model="orderId"
+      ref="orderInput"
+    />
+    <p class="text-left text-xl font-bold text-red-500" v-if="!isOrderIdInputDisabled">
+      *訂單編號不足20碼
+    </p>
+    <button
+      type="button"
+      class="text-primary mx-auto my-5 block w-full rounded-xl px-4 py-1 text-2xl"
+      :class="btnClass"
+      @click="getOeder"
+    >
+      送出
+    </button>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
