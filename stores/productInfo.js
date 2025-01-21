945bc6f6d4ef7c4e4dc91b67769a40155bd88537
mizu <psdk55052004@gmail.com>
2025-01-22 01:14:43 +0800

    新增 單一產品頁面 store

---
+++ stores/productInfo.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+
+export const useProductInfoStore = defineStore("useProductInfoStore", () => {
+  const config = useRuntimeConfig();
+  const productInfoData = ref({});
+
+  function getProductInfo(product) {
+    try {
+      productInfoData.value = JSON.parse(JSON.stringify(product));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  async function fetchProductInfo(productId) {
+    if (!productId) return;
+
+    const { apiPath, apiBaseUrl } = config.public;
+    const api = `${apiBaseUrl}/api/${apiPath}/product/${productId}`;
+    try {
+      const res = await $fetch(api);
+      productInfoData.value = res.product;
+      console.log("productInfoData.value: ", productInfoData.value);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return { productInfoData, getProductInfo, fetchProductInfo };
+});
