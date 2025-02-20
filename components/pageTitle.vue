23528e8b1872e3e9a638850128c1d6594ee26994
mizu <psdk55052004@gmail.com>
2025-02-20 23:42:34 +0800

    新增 頁面標題元件 響應式

--- components/pageTitle.vue
+++ components/pageTitle.vue
@@ -11,7 +11,7 @@ const props = defineProps({
   <div class="text-primary">
     <h2 class="mb-12 text-center">
       <span
-        class="before:bg-primary after:bg-primary relative text-2xl before:absolute before:right-full before:top-1/2 before:h-[1px] before:w-[120px] before:-translate-x-16 after:absolute after:left-full after:top-1/2 after:h-[1px] after:w-[120px] after:translate-x-16"
+        class="before:bg-primary after:bg-primary relative text-2xl before:absolute before:right-full before:top-1/2 before:h-[1px] before:w-[60px] before:-translate-x-8 after:absolute after:left-full after:top-1/2 after:h-[1px] after:w-[60px] after:translate-x-8 lg:before:w-[120px] lg:before:-translate-x-16 lg:after:w-[120px] lg:after:translate-x-16"
         >{{ props.title }}</span
       >
     </h2>
